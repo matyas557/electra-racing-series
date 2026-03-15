70081c1039a8da92fdf672972c95a10f81650c68
matyas557 <vosmat58@gasos-ro.cz>
2026-03-15 22:07:49 +0100

    Update-3

---
+++ client/src/components/FeatureSection.tsx
@@ -0,0 +1,13 @@
+/*
+ * DESIGN: Night Race Cinematic
+ * Feature: Asymmetric two-column layout, alternating image/text sides
+ * Uses pit stop and podium images with dramatic overlays
+ */
+
+const PIT_STOP_IMG = "https://d2xsxph8kpxj0f.cloudfront.net/310519663419012309/FUBNiPFctLLWzH3m2BbTut/f1-pitstop-6LekPe5kBVmXjtigjaqT5E.webp";
+const PODIUM_IMG = "https://d2xsxph8kpxj0f.cloudfront.net/310519663419012309/FUBNiPFctLLWzH3m2BbTut/f1-podium-Kseqg7oy4CR4FG6bWAW64o.webp";
+const CAR_IMG = "https://d2xsxph8kpxj0f.cloudfront.net/310519663419012309/FUBNiPFctLLWzH3m2BbTut/f1-car-closeup-cXWGnyZycDZhL5NsZv8zWA.webp";
+
+export default function FeatureSection() {
+  
+}
